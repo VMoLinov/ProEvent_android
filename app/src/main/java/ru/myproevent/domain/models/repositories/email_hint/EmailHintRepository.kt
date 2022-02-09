29a7a2f861904249c4ea6e984b986e2e22149826
kusch ivan <i-kush@mail.ru>
2022-02-09 08:19:33 +0700

    PROEV-291 ANDROID сделать подсказки email-а для всех форм где можно вводить email (Экран Авторизация и экран Безопасность) по аналогии с тем как это реализована на экране Регистрации

--- app/src/main/java/ru/myproevent/domain/models/repositories/email_hint/EmailHintRepository.kt
+++ app/src/main/java/ru/myproevent/domain/models/repositories/email_hint/EmailHintRepository.kt
@@ -3,11 +3,13 @@ package ru.myproevent.domain.models.repositories.email_hint
 import io.reactivex.Single
 import io.reactivex.schedulers.Schedulers
 import ru.myproevent.domain.models.HintRequest
+import ru.myproevent.domain.models.IEmailHintDataSource
 import ru.myproevent.domain.models.IProEventDataSource
 import ru.myproevent.domain.models.Suggestion
 import javax.inject.Inject
 
-class EmailHintRepository @Inject constructor(private val api: IProEventDataSource):IEmailHintRepository {
+class EmailHintRepository @Inject constructor(private val api: IEmailHintDataSource) :
+    IEmailHintRepository {
     override fun getEmailHint(part_email: String): Single<List<Suggestion>> {
         return api.getEmailHint(HintRequest(part_email)).map { it.suggestions }
             .subscribeOn(Schedulers.io())
