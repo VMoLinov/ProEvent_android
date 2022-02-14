609482bddd69bf89a4dd21ad198afc78a57321a8
GrishaninVyacheslav <64549372+GrishaninVyacheslav@users.noreply.github.com>
2022-02-14 21:45:47 +0300

    @wip

--- app/src/main/java/ru/myproevent/ui/views/ProEventEditOptions.kt
+++ app/src/main/java/ru/myproevent/ui/views/ProEventEditOptions.kt
@@ -1,15 +1,12 @@
 package ru.myproevent.ui.views
 
 import android.content.Context
-import android.graphics.Typeface
 import android.util.AttributeSet
-import android.view.Gravity
-import android.view.ViewGroup
+import android.view.LayoutInflater
 import android.widget.FrameLayout
-import android.widget.TextView
+import android.widget.Toast
 import ru.myproevent.ProEventApp
-import ru.myproevent.R
-import ru.myproevent.domain.utils.pxValue
+import ru.myproevent.databinding.ItemEditOptionsBinding
 
 class ProEventEditOptions : FrameLayout {
     constructor(context: Context) : this(context, null)
@@ -18,60 +15,15 @@ class ProEventEditOptions : FrameLayout {
         context,
         attrs,
         defStyleAttr
-    )
-
-    private lateinit var cancelViewParams: ViewGroup.LayoutParams
-    private lateinit var saveViewParams: ViewGroup.LayoutParams
-
-    // TODO: отрефакторить: возможно стоит создавать childViews не в самой ProEventEditOptions, а определить их в виде xml layout-а и надувать его отсюда
-    private val cancelView = TextView(context).apply {
-        text = "Отменить"
-        // TODO: отрефакторить: передавать это как стиль через AttributeSet
-        setTextColor(ProEventApp.instance.resources.getColor(R.color.ProEvent_blue_800, null))
-        textSize = 20F
-        typeface = Typeface.createFromAsset(
-            ProEventApp.instance.assets,
-            ProEventApp.instance.getString(R.string.default_font_bold_path)
-        )
+    ){
+        addView(binding.root)
+        initViews()
     }
 
-    private val saveView = TextView(context).apply {
-        text = "Сохранить"
-        // TODO: отрефакторить: передавать это как стиль через AttributeSet
-        setTextColor(ProEventApp.instance.resources.getColor(R.color.ProEvent_blue_800, null))
-        textSize = 20F
-        typeface = Typeface.createFromAsset(
-            ProEventApp.instance.assets,
-            ProEventApp.instance.getString(R.string.default_font_bold_path)
-        )
-    }
+    private val binding = ItemEditOptionsBinding.inflate(LayoutInflater.from(context))
 
-    private fun updateContent() {
-        removeAllViews()
-        addView(cancelView, cancelViewParams)
-        addView(saveView, saveViewParams)
-        invalidate()
-    }
-
-    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-        cancelViewParams = LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT).apply {
-            setMargins(
-                pxValue(20f).toInt(),
-                0,
-                0,
-                0
-            )
-        }
-        saveViewParams = LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT).apply {
-            setMargins(
-                0,
-                0,
-                pxValue(20f).toInt(),
-                0
-            )
-            gravity = Gravity.RIGHT
-        }
-        updateContent()
+    private fun initViews() = with(binding){
+        cancel.setOnClickListener { Toast.makeText(ProEventApp.instance, "ОТМЕНА ИЗМЕНЕНИЙ", Toast.LENGTH_SHORT).show() }
+        save.setOnClickListener { Toast.makeText(ProEventApp.instance, "СОХРАНЕНИЕ ИЗМЕНЕНИЙ", Toast.LENGTH_SHORT).show() }
     }
 }
