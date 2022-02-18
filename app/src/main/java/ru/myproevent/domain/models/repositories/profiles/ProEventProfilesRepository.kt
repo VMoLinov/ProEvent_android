4f7383a9df1de957c235ad5c7677909f21212fa0
VMolinov <molinov@list.ru>
2022-02-18 15:32:07 +0300

    PROEV-260

--- app/src/main/java/ru/myproevent/domain/models/repositories/profiles/ProEventProfilesRepository.kt
+++ app/src/main/java/ru/myproevent/domain/models/repositories/profiles/ProEventProfilesRepository.kt
@@ -4,20 +4,19 @@ import io.reactivex.Completable
 import io.reactivex.Single
 import io.reactivex.schedulers.Schedulers
 import retrofit2.HttpException
+import retrofit2.Response
 import ru.myproevent.domain.models.ContactDto
 import ru.myproevent.domain.models.IProEventDataSource
 import ru.myproevent.domain.models.ProfileIdListDto
 import ru.myproevent.domain.models.entities.Contact
 import ru.myproevent.domain.models.entities.Contact.Status
 import ru.myproevent.domain.models.entities.Profile
-import ru.myproevent.domain.models.repositories.images.IImagesRepository
 import ru.myproevent.domain.utils.toContact
 import ru.myproevent.domain.utils.toProfile
 import javax.inject.Inject
 
 class ProEventProfilesRepository @Inject constructor(
-    private val api: IProEventDataSource,
-    private val imagesRepository: IImagesRepository
+    private val api: IProEventDataSource
 ) : IProEventProfilesRepository {
 
     override fun getProfile(id: Long): Single<Profile?> = Single.fromCallable {
@@ -36,46 +35,13 @@ class ProEventProfilesRepository @Inject constructor(
         throw HttpException(response)
     }.subscribeOn(Schedulers.io())
 
-    // TODO: ошибки здесь обрабатывабтся не правильно
     override fun saveProfile(profile: Profile): Completable =
         Completable.fromCallable {
             val oldProfileResponse = api.getProfile(profile.id).execute()
-            val newProfileResponse =
-                if (oldProfileResponse.isSuccessful) {
-                    // TODO: это штука могла быть не Successful не только потому что профиля нет
-                    val oldProfile = oldProfileResponse.body()!!
-                    if (profile.email == null) {
-                        profile.email = oldProfile.email
-                    }
-                    if (profile.fullName == null) {
-                        profile.fullName = oldProfile.fullName
-                    }
-                    if (profile.nickName == null) {
-                        profile.nickName = oldProfile.nickName
-                    }
-                    if (profile.phone == null) {
-                        profile.phone = oldProfile.phone
-                    }
-                    if (profile.position == null) {
-                        profile.position = oldProfile.position
-                    }
-                    if (profile.birthdate == null) {
-                        profile.birthdate = oldProfile.birthdate
-                    }
-                    if (profile.description == null) {
-                        profile.description = oldProfile.description
-                    }
-                    if (profile.imgUri.isNullOrEmpty()) {
-                        profile.imgUri = oldProfile.imgUri
-                    }
-                    api.editProfile(profile).execute()
-                } else {
-                    api.createProfile(profile).execute()
-                }
+            val newProfileResponse = compareProfiles(oldProfileResponse, profile)
             if (!newProfileResponse.isSuccessful) {
                 throw HttpException(newProfileResponse)
             }
-            return@fromCallable null
         }.subscribeOn(Schedulers.io())
 
     override fun getContact(contactDto: ContactDto): Single<Contact> {
@@ -87,5 +53,19 @@ class ProEventProfilesRepository @Inject constructor(
             }
             throw HttpException(response)
         }.subscribeOn(Schedulers.io())
+    }
+
+    private fun compareProfiles(
+        oldProfileResponse: Response<Profile>,
+        profile: Profile
+    ): Response<Profile> {
+        return if (oldProfileResponse.isSuccessful && oldProfileResponse.body() != null) {
+            val oldProfile = oldProfileResponse.body()!!
+            api.editProfile(profile.merge(oldProfile)).execute()
+        } else if (oldProfileResponse.errorBody() != null) {
+            throw HttpException(oldProfileResponse)
+        } else {
+            api.createProfile(profile).execute()
+        }
     }
 }
